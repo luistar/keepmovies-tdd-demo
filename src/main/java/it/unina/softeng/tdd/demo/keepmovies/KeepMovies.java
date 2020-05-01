b0a59d4def53fa1930d5651f2f1847d13b97426d
Luigi Starace <luigi.starace@gmail.com>
2020-05-01 15:24:50 +0200

    Made our first test compile.
    Just did as little as possible to make it compile. Now we can see it
    fail (expected empty collection, but got null).

--- src/main/java/it/unina/softeng/tdd/demo/keepmovies/KeepMovies.java
+++ src/main/java/it/unina/softeng/tdd/demo/keepmovies/KeepMovies.java
@@ -1,5 +1,12 @@
 package it.unina.softeng.tdd.demo.keepmovies;
 
+import java.util.List;
+
 public class KeepMovies {
+
+	public List<Movie> getMovies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
