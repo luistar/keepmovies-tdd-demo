d8074dfd2c6e279e78cf63ba010a2eac03632b41
Luigi Starace <luigi.starace@gmail.com>
2020-05-01 17:10:17 +0200

    Added failing test for the "be able to mark a movie as watched" feature

--- src/test/java/it/unina/softeng/tdd/demo/keepmovies/MovieSpecification.java
+++ src/test/java/it/unina/softeng/tdd/demo/keepmovies/MovieSpecification.java
@@ -1,10 +1,18 @@
 package it.unina.softeng.tdd.demo.keepmovies;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Year;
+
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 class MovieSpecification {
 
+	@Test
+	void shouldBeMarkedAsNotWatchedByDefault() {
+		Movie m = new Movie("1917",Year.of(2019),"war");
+		assertThat(m.isAlreadyWatched(), is(false));
+	}
 }
